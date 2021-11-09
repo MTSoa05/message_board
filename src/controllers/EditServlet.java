c14b271960b2fae9da1dea5dd0daf724f4a8b2a9
ryoga.matsuo <matsuo.3110@gmail.com>
2021-11-09 16:50:23 +0900

    UpdateServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -40,7 +40,10 @@ public class EditServlet extends HttpServlet {
 
         // メッセージ情報とセッションIDをリクエストスコープに登録
         request.setAttribute("message", m);
-        request.setAttribute("_tpken", request.getSession().getId());
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("message_id", m.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
